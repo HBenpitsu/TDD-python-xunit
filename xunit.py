3e9dc142ee6c7a906f034b584da2b6cad013d10f
hbenpitsu <hbenpitsu15@gmail.com>
2025-03-31 13:20:09 +0900

    runも引き上げる - 動的な呼び出しの責務を抽出できた

--- xunit.py
+++ xunit.py
@@ -1,14 +1,14 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def run(self):
+        targetMethod = getattr(self, self.name)
+        targetMethod()
 
 class WasRun(TestCase):
     def __init__(self, name):
         super().__init__(name)
         self.wasRun = False
-    def run(self):
-        targetMethod = getattr(self, self.name)
-        targetMethod()
     def testMethod(self):
         self.wasRun = True
 
