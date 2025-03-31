8a06d2bab4156d1982875755c449c644ccf4c787
hbenpitsu <hbenpitsu15@gmail.com>
2025-03-31 14:14:56 +0900

    クラス整理する

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import xunit
 
-class WasRun(xunit.TestCase):
+class TestCaseSample(xunit.TestCase):
     def __init__(self, name):
         super().__init__(name)
         self.log = ""
@@ -10,27 +10,21 @@ class WasRun(xunit.TestCase):
         self.log += "testMethod "
     def tearDown(self):
         self.log += "tearDown "
-
-class HaveResult(xunit.TestCase):
-    def __init__(self, name):
-        super().__init__(name)
-    def testMethod(self):
-        pass
     def testBrokenMethod(self):
         raise Exception("Broken Method")
 
 class TestCaseTest(xunit.TestCase):
     def testRunning(self):
-        wasRunTest = WasRun("testMethod")
+        wasRunTest = TestCaseSample("testMethod")
         assert wasRunTest.log == ""
         wasRunTest.run()
         assert wasRunTest.log == "setUp testMethod tearDown "
     def testResult(test):
-        resultTest = HaveResult("testMethod")
+        resultTest = TestCaseSample("testMethod")
         result = resultTest.run()
         assert result.summary() == "1 run, 0 failed"
     def testFailedResult(test):
-        resultTest = HaveResult("testBrokenMethod")
+        resultTest = TestCaseSample("testBrokenMethod")
         result = resultTest.run()
         assert result.summary() == "1 run, 1 failed"
 
