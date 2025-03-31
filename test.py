f9fdd427fa3cfd8b03e97ce2d7ae58801e887b18
hbenpitsu <hbenpitsu15@gmail.com>
2025-03-31 13:53:13 +0900

    不要なフィクスチャをインライン化する

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import xunit
 
 class TestCaseTest(xunit.TestCase):
-    def setUp(self):
-        self.wasRunTest = xunit.WasRun("testMethod")
     def testRunning(self):
-        assert self.wasRunTest.log == ""
-        self.wasRunTest.run()
-        assert self.wasRunTest.log == "setUp testMethod tearDown "
+        wasRunTest = xunit.WasRun("testMethod")
+        assert wasRunTest.log == ""
+        wasRunTest.run()
+        assert wasRunTest.log == "setUp testMethod tearDown "
 
 TestCaseTest("testRunning").run()
