351563fa92402e95a31857e6b7fa36f6a3e916a3
hbenpitsu <hbenpitsu15@gmail.com>
2025-03-31 14:00:56 +0900

    失敗するテストを書く

--- test.py
+++ test.py
@@ -11,11 +11,22 @@ class WasRun(xunit.TestCase):
     def tearDown(self):
         self.log += "tearDown "
 
+class HaveResult(xunit.TestCase):
+    def __init__(self, name):
+        super().__init__(name)
+    def testMethod(self):
+        pass
+
 class TestCaseTest(xunit.TestCase):
     def testRunning(self):
         wasRunTest = WasRun("testMethod")
         assert wasRunTest.log == ""
         wasRunTest.run()
         assert wasRunTest.log == "setUp testMethod tearDown "
+    def testResult(test):
+        resultTest = HaveResult("testMethod")
+        result = resultTest.run()
+        assert result.summary() == "1 run, 0 failed"
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testResult").run()
