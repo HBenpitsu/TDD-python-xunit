54653879cd645aa25310dfdc4ccddf5424e5e446
hbenpitsu <hbenpitsu15@gmail.com>
2025-03-31 13:44:42 +0900

    とにかくテストを通す(明白な実装)

--- xunit.py
+++ xunit.py
@@ -7,13 +7,19 @@ class TestCase:
         targetMethod = getattr(self, self.name)
         self.setUp()
         targetMethod()
+        self.tearDown()
+    def tearDown(self):
+        pass
 
 class WasRun(TestCase):
     def __init__(self, name):
         super().__init__(name)
         self.wasRun = False
         self.wasSetUp = False
+        self.wasTearedDown = False
     def setUp(self):
         self.wasSetUp = True
     def testMethod(self):
         self.wasRun = True
+    def tearDown(self):
+        self.wasTearedDown = True
