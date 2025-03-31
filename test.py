a5018d51c7b2ac367561f6e62f8faea11d7c7ca0
hbenpitsu <hbenpitsu15@gmail.com>
2025-03-31 13:52:08 +0900

    リファクタリング

--- test.py
+++ test.py
@@ -7,15 +7,5 @@ class TestCaseTest(xunit.TestCase):
         assert self.wasRunTest.log == ""
         self.wasRunTest.run()
         assert self.wasRunTest.log == "setUp testMethod tearDown "
-    def testWasSetUp(self):
-        assert self.wasRunTest.log == ""
-        self.wasRunTest.run()
-        assert self.wasRunTest.log == "setUp testMethod tearDown "
-    def testWasTearedDown(self):
-        assert self.wasRunTest.log == ""
-        self.wasRunTest.run()
-        assert self.wasRunTest.log == "setUp testMethod tearDown "
 
 TestCaseTest("testRunning").run()
-TestCaseTest("testWasSetUp").run()
-TestCaseTest("testWasTearedDown").run()
